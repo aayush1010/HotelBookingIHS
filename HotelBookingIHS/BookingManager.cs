99c13c9a29781e0086a3fffe041f895b81a8c32e
Aayush Mittal <Aayush.Mittal@Blackstone.com>
2021-07-30 22:10:52 +0530

    Booking Manager Implementation

---
+++ HotelBookingIHS/BookingManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HotelBookingIHS
+{
+    public class BookingManager : IBookingManager
+    {
+        Dictionary<DateTime, HashSet<int>> BookedRoomNumbers;
+        HashSet<int> ValidRoomIds;
+        private object obj = new object();
+
+        public BookingManager()
+        {
+            ValidRoomIds = new HashSet<int> { 101, 102, 201, 203 };
+            BookedRoomNumbers = new Dictionary<DateTime, HashSet<int>>();
+
+        }
+
+        public void AddBooking(string guest, int room, DateTime date)
+        {
+
+            if (ValidRoomIds.Contains(room))
+            {
+                lock (obj)
+                {
+                    if (!BookedRoomNumbers.ContainsKey(date.Date))
+                    {
+                        BookedRoomNumbers.Add(date.Date, new HashSet<int> { room });
+                    }
+                    else if (BookedRoomNumbers[date.Date].Contains(room))
+                    {
+                        throw new Exception("Room already booked");
+                    }
+                    else
+                    {
+                        BookedRoomNumbers[date.Date].Add(room);
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Invalid Room Number");
+            }
+
+        }
+
+        public IEnumerable<int> GetAvailableRooms(DateTime date)
+        {
+            var bookedRooms = BookedRoomNumbers.ContainsKey(date.Date) ? BookedRoomNumbers[date.Date] : new HashSet<int>();
+            return ValidRoomIds.Where(x => !bookedRooms.Contains(x));
+        }
+
+        public bool IsRoomAvailable(int room, DateTime date)
+        {
+            return !BookedRoomNumbers.ContainsKey(date.Date) || !BookedRoomNumbers[date.Date].Contains(room);
+        }
+    }
+}
---
+++ HotelBookingIHS/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HotelBookingIHS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IBookingManager bm = new BookingManager();// create your manager here; 
+            var today = new DateTime(2012, 3, 28);
+            Console.WriteLine(bm.IsRoomAvailable(101, today)); // outputs true 
+            bm.AddBooking("Patel", 101, today);
+            Console.WriteLine(bm.IsRoomAvailable(101, today)); // outputs false 
+            bm.AddBooking("Li", 101, today); // 
+            var availableRooms = bm.GetAvailableRooms(today);
+            foreach (var room in availableRooms)
+            {
+                Console.WriteLine(room);
+
+            }
+        }
+    }
+}
